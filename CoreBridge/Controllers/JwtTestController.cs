497584035a1256c963cf6267863a9f8daf9e71c8
Satoshi Notake <satoshi.notake@scopenext.jp>
2022-11-11 10:56:10 +0900

    テスト用コントローラー追加忘れ

---
+++ CoreBridge/Controllers/JwtTestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CoreBridge.Models.lib;
+
+namespace CoreBridge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JwtTestController : ControllerBase
+    {
+        private JWTManager _jwtcontent = new JWTManager();
+
+        [HttpGet]
+        public async Task<ActionResult<string>> GetJWTtems()
+        {
+            //テストクラスで実験.
+            PayloadSample payloadSample = new PayloadSample();
+            payloadSample.foo = "foo";
+            payloadSample.bar = "bar";
+
+            //Tokenを作る
+            _jwtcontent.CreateToken(payloadSample);
+
+            //TokenをDecode
+            return await _jwtcontent.DecToken();
+        }
+
+
+
+    }
+
+}
