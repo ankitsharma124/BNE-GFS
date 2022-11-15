8cffc43bd79c97d174764c0fb503129d8938314f
tetuyoko <tyokoyama53@gmail.com>
2022-11-15 12:59:53 +0900

    feat(tf): Add terraform configurations.

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  project = "corebridge-367900"
+  region  = "asia-northeast1"
+  zone    = "asia-northeast1-a"
+}
+
+terraform {
+ backend "gcs" {
+   bucket  = "tf-state-934af1408b5cdc78" # google_storage_bucket.tfstate.name
+   prefix  = "terraform/state"
+ }
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "tfstate" {
+  name          = "tf-state-${random_id.bucket_suffix.hex}"
+  force_destroy = false
+  location      = "ASIA-NORTHEAST1"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
