b368453e18dc8bef64b47780f7aec0933df9e85a
tetuyoko <tyokoyama53@gmail.com>
2022-11-16 18:49:00 +0900

    feat(tf): Add CloudRun

--- terraform/memorystore.tf
+++ terraform/memorystore.tf
@@ -1,12 +1,12 @@
 # NOTE:
 #  Use standard tier for production.
 resource "google_redis_instance" "corebridge" {
-  name                    = "corebridge"
-  display_name            = "corebridge"
-  memory_size_gb          = 2
-  redis_version           = "REDIS_6_X"
-  tier                    = "BASIC"
-  authorized_network      = "default"
+  name               = "corebridge"
+  display_name       = "corebridge"
+  memory_size_gb     = 2
+  redis_version      = "REDIS_6_X"
+  tier               = "BASIC"
+  authorized_network = "default"
 
   persistence_config {
     persistence_mode = "DISABLED"
