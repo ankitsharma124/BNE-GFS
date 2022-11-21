853593a5b8ff4eeb0edf49db58d9885227a15abd
tetuyoko <tyokoyama53@gmail.com>
2022-11-21 13:10:08 +0900

    feat(tf): Normalize terraform action

--- terraform/artifact_registry.tf
+++ terraform/artifact_registry.tf
@@ -9,28 +9,10 @@ resource "google_artifact_registry_repository" "corebridge" {
   description   = "corebridge docker repository"
   format        = "DOCKER"
 
-  labels = local.default_labels
-
+  labels     = local.default_labels
   depends_on = [google_project_service.artifactregistry]
 }
 
-
-resource "google_service_account" "registry_admin" {
-  account_id   = "registry-admin"
-  display_name = "Service Account to manage images on registry"
-}
-
-resource "google_artifact_registry_repository_iam_member" "registry_admin" {
-  location   = google_artifact_registry_repository.corebridge.location
-  repository = google_artifact_registry_repository.corebridge.name
-  role       = "roles/artifactregistry.repoAdmin"
-  member     = "serviceAccount:${google_service_account.registry_admin.email}"
-}
-
-#output "registry_service_account_email" {
-#  description = "service account for registry"
-#  value       = google_service_account.registry_admin.email
-#}
 
 output "registry_corebridge_host" {
   description = "you may need to run 'gcloud auth configure-docker <this value>.'"
--- terraform/github_actions.tf
+++ terraform/github_actions.tf
@@ -2,24 +2,6 @@ locals {
   github_repo = "scopenext/CoreBridge"
 }
 
-#module "gh_oidc" {
-#  source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
-#  project_id  = local.project
-#  pool_id     = "github-pool"
-#  provider_id = "github-pool-provider"
-#  sa_mapping = {
-#    (google_service_account.registry_admin.id) = {
-#      sa_name   = google_service_account.registry_admin.name
-#      attribute = "attribute.repository/${local.github_repo}"
-#    }
-#  }
-#}
-#
-#output "github_actions_workload_identity_pool_provider" {
-#  description = "Workload Identity Pool Provider ID"
-#  value       = module.gh_oidc.provider_name
-#}
-
 
 resource "google_service_account" "terraform" {
   account_id = "terraform-sa"
