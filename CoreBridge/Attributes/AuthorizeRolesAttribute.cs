34ec1144b49e4e70ba2386ae5bb4832163ef17b7
Satoshi Notake <satoshi.notake@scopenext.jp>
2022-12-21 17:08:53 +0900

    追加ファイル登録

---
+++ CoreBridge/Attributes/AuthorizeRolesAttribute.cs
@@ -0,0 +1,14 @@
+﻿using CoreBridge.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CoreBridge.Attributes
+{
+    public class AuthorizeRolesAttribute : AuthorizeAttribute
+    {
+
+        public AuthorizeRolesAttribute(params AdminUserRoleEnum[] roles)
+        {
+            Roles = string.Join(", ", roles);
+        }
+    }
+}
