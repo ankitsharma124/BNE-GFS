3d9485cc8bc343e6be9aa1909633295cade2a417
Satoshi Notake <satoshi.notake@scopenext.jp>
2022-12-21 17:01:39 +0900

    ゲーム制作ユーザー側登録のサインイン・ロール追加処理の場所を変更（登録時）

--- CoreBridge/Controllers/AppUserRegisterController.cs
+++ CoreBridge/Controllers/AppUserRegisterController.cs
@@ -55,40 +55,141 @@ namespace CoreBridge.Controllers
         [HttpPost]
         public async Task<IActionResult> Confirm(AppUserDto dto)
         {
+            //タイトルコードの存在は確認する.
+            if (dto.TitleCode != null)
+            {
+                //タイトルコードの存在は確認する.
+                var check = await _titleInfoService.FindTitleCode(dto.TitleCode);
+                if (check == false)
+                {
+                    string errorMsg = "同一のタイトルコードがあませんでした！登録済みタイトルコードを利用してください。";
+                    ViewBag.Alert = errorMsg;
+                    ModelState.AddModelError(string.Empty, errorMsg);
+                    return View(dto);
+                }
+            }
+
+            if (dto.Password != dto.ConfirmPassword)
+            {
+                //エラーメッセージ
+                string errorMsg = "パスワードが一致しませんでした。一致するパスワードを使用してください";
+                ViewBag.Alert = errorMsg;
+
+                ModelState.AddModelError(string.Empty, errorMsg);
+                return View(dto);
+            }
+
+            //var check = await _titleInfoService.FindTitleCode(dto.TitleCode);
+            //if (check == false)
+            //{
+            //    string errorMsg = "同一のタイトルコードがあませんでした！登録済みタイトルコードを利用してください。";
+            //    ViewBag.Alert = errorMsg;
+            //    ModelState.AddModelError(string.Empty, errorMsg);
+            //    //return View();
+            //}
+
+            if (dto.IsValid())
+            {
+                return View(dto);
+            }
+            else
+            {
+                return View("/");
+            }
+
+            //string returnUrl = Url.Content("~/AppUserRegister");
+
+            //if (ModelState.IsValid)
+            //{
+            //    if (dto.TitleCode != null)
+            //    {
+            //        //タイトルコードの存在は確認する.
+            //        //var check = await _appUserService.FindTitleCode(dto.TitleCode);
+            //        var check = await _titleInfoService.FindTitleCode(dto.TitleCode);
+            //        if (check == false)
+            //        {
+            //            //エラーメッセージ
+            //            //string errorMsg = "同一のタイトルコードがありました！一意のものを使用してください";
+            //            //ViewBag.Alert = errorMsg;
+
+            //            //ModelState.AddModelError(string.Empty, errorMsg);
+            //            //return View(dto);
+
+            //            string errorMsg = "同一のタイトルコードがあませんでした！登録済みタイトルコードを利用してください。";
+            //            ViewBag.Alert = errorMsg;
+            //            ModelState.AddModelError(string.Empty, errorMsg);
+            //            return View();
+            //        }
+            //    }
+
+            //    if(dto.Password != dto.ConfirmPassword)
+            //    {
+            //        //エラーメッセージ
+            //        string errorMsg = "パスワードが一致しませんでした。一致するパスワードを使用してください";
+            //        ViewBag.Alert = errorMsg;
+
+            //        ModelState.AddModelError(string.Empty, errorMsg);
+            //        return View(dto);
+            //    }
+
+            //    var user = CreateUser();
+
+            //    await _userStore.SetUserNameAsync(user, dto.UserId, CancellationToken.None);
+            //    //await _emailStore.SetEmailAsync(user, dto.EMail, CancellationToken.None);
+            //    var result = await _userManager.CreateAsync(user, dto.Password);
+
+            //    if (result.Succeeded)
+            //    {
+            //        _logger.LogInformation("User created a new account with password.");
+
+            //        var userId = await _userManager.GetUserIdAsync(user);
+            //        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+            //        //ロール管理追加.
+            //        await CheckAppRole(dto.Role.ToString());
+            //        var role = await _userManager.AddToRoleAsync(user, dto.Role.ToString());
+
+            //        return View(dto);
+            //    }
+            //    foreach (var error in result.Errors)
+            //    {
+            //        ModelState.AddModelError(string.Empty, error.Description);
+            //    }
+            //}
+
+            //return View(dto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AppUserDto dto)
+        {
             string returnUrl = Url.Content("~/AppUserRegister");
 
             if (ModelState.IsValid)
             {
-                if (dto.TitleCode != null)
-                {
-                    //タイトルコードの存在は確認する.
-                    //var check = await _appUserService.FindTitleCode(dto.TitleCode);
-                    var check = await _titleInfoService.FindTitleCode(dto.TitleCode);
-                    if (check == false)
-                    {
-                        //エラーメッセージ
-                        //string errorMsg = "同一のタイトルコードがありました！一意のものを使用してください";
-                        //ViewBag.Alert = errorMsg;
+                //if (dto.TitleCode != null)
+                //{
+                //    //タイトルコードの存在は確認する.
+                //    //var check = await _appUserService.FindTitleCode(dto.TitleCode);
+                //    var check = await _titleInfoService.FindTitleCode(dto.TitleCode);
+                //    if (check == false)
+                //    {
+                //        string errorMsg = "同一のタイトルコードがあませんでした！登録済みタイトルコードを利用してください。";
+                //        ViewBag.Alert = errorMsg;
+                //        ModelState.AddModelError(string.Empty, errorMsg);
+                //        return View();
+                //    }
+                //}
 
-                        //ModelState.AddModelError(string.Empty, errorMsg);
-                        //return View(dto);
+                //if (dto.Password != dto.ConfirmPassword)
+                //{
+                //    //エラーメッセージ
+                //    string errorMsg = "パスワードが一致しませんでした。一致するパスワードを使用してください";
+                //    ViewBag.Alert = errorMsg;
 
-                        string errorMsg = "同一のタイトルコードがあませんでした！登録済みタイトルコードを利用してください。";
-                        ViewBag.Alert = errorMsg;
-                        ModelState.AddModelError(string.Empty, errorMsg);
-                        return View();
-                    }
-                }
-
-                if(dto.Password != dto.ConfirmPassword)
-                {
-                    //エラーメッセージ
-                    string errorMsg = "パスワードが一致しませんでした。一致するパスワードを使用してください";
-                    ViewBag.Alert = errorMsg;
-
-                    ModelState.AddModelError(string.Empty, errorMsg);
-                    return View(dto);
-                }
+                //    ModelState.AddModelError(string.Empty, errorMsg);
+                //    return View(dto);
+                //}
 
                 var user = CreateUser();
 
@@ -107,6 +208,8 @@ namespace CoreBridge.Controllers
                     await CheckAppRole(dto.Role.ToString());
                     var role = await _userManager.AddToRoleAsync(user, dto.Role.ToString());
 
+                    await _appUserService.GenerateAppUser(dto);
+
                     return View(dto);
                 }
                 foreach (var error in result.Errors)
@@ -116,21 +219,17 @@ namespace CoreBridge.Controllers
             }
 
             return View(dto);
-        }
 
-        [HttpPost]
-        public async Task<IActionResult> Create(AppUserDto dto)
-        {
 
-            if (dto.IsValid())
-            {
-                await _appUserService.GenerateAppUser(dto);
-                return View(dto);
-            }
-            else
-            {
-                return View("/");
-            }
+            //if (dto.IsValid())
+            //{
+            //    await _appUserService.GenerateAppUser(dto);
+            //    return View(dto);
+            //}
+            //else
+            //{
+            //    return View("/");
+            //}
         }
 
         private IdentityUser CreateUser()
