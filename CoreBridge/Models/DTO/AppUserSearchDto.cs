1734a1170887c6c11be5900e309b41306ba384c2
Satoshi Notake <satoshi.notake@scopenext.jp>
2022-12-29 13:32:22 +0900

    ファイル追加

---
+++ CoreBridge/Models/DTO/AppUserSearchDto.cs
@@ -0,0 +1,24 @@
+﻿namespace CoreBridge.Models.DTO
+{
+    public class AppUserSearchDto
+    {
+        public AppUserSearchDto() { }
+
+        public AppUserSearchDto(string? userId, AdminUserRoleEnum? userRole, string? titleCode, DateTime? createdDate, DateTime? updatedDate, bool resetData)
+        {
+            UserId = userId;
+            UserRole = userRole;
+            TitleCode = titleCode;
+            CreatedDate = createdDate;
+            UpdatedDate = updatedDate;
+            ResetData = resetData;
+        }
+
+        public string? UserId { get; set; }
+        public AdminUserRoleEnum? UserRole { get; set; }
+        public string? TitleCode { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public bool ResetData { get; set; }
+    }
+}
