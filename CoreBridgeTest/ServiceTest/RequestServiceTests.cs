8e5ed7f80e714fa4757057500563bd060f7be043
scopenext-ankit <sharma.ankit.kumar@scopenext.jp>
2023-11-07 17:01:34 +0900

    final commit

---
+++ CoreBridgeTest/ServiceTest/RequestServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using CoreBridge.Models.DTO.Requests;
+using CoreBridge.Services;
+using CoreBridge.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+using XAct;
+
+namespace CoreBridgeTest.ServiceTest
+{
+	public class RequestServiceTests
+	{
+        Mock<IConfiguration> mockConfig;
+        Mock<ISessionStatusService> mockSessionStatusService;
+        Mock<ILogger<RequestService>> mockLogger;
+        Mock<ISessionDataService> mockSessionDataService;
+        Mock<IUserService> mockUserService;
+        Mock<IMaintenanceService> mockMaintenanceService;
+        Mock<IHashService> mockHashService;
+        Mock<ITitleInfoService> mockTitleInfoService;
+        RequestService requestService;
+
+        public RequestServiceTests()
+		{
+            mockConfig = new Mock<IConfiguration>();
+            mockSessionStatusService = new Mock<ISessionStatusService>();
+            mockSessionDataService = new Mock<ISessionDataService>();
+            mockLogger = new Mock<ILogger<RequestService>>();
+            mockUserService = new Mock<IUserService>();
+            mockMaintenanceService = new Mock<IMaintenanceService>();
+            mockHashService = new Mock<IHashService>();
+            mockTitleInfoService = new Mock<ITitleInfoService>();
+            requestService = new RequestService(mockConfig.Object, mockSessionStatusService.Object, mockTitleInfoService.Object,
+                                                                        mockSessionDataService.Object, mockUserService.Object, mockMaintenanceService.Object,
+                                                                        mockHashService.Object, mockLogger.Object);
+		}
+
+        [Fact]
+        public async Task ProcessRequest_ValidRequest_Success()
+        {
+            // Arrange
+            var mockRequest = new Mock<HttpRequest>();
+            var req = mockRequest.Object;
+
+            var mockRequestHeader = new Mock<ReqBase>();
+            var reqHeader = mockRequestHeader.Object;
+
+            var reqParam = new ReqBaseParam();
+
+            // Act
+            Assert.Throws(await requestService.ProcessRequest(req, reqHeader, reqParam));
+
+            // Assert
+            // Add asserts to verify the behavior of the methods called in the ProcessRequest method
+        }
+
+    }
+}
+
